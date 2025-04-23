22884787fa25d1b5c35f20dca16d5f0a82a7994d
frodkata <frodkata@gmail.com>
2025-04-23 17:28:55 +0300

    Refactor styles: move character page styles to characterPage.css and update imports

--- src/pages/CharacterPage.tsx
+++ src/pages/CharacterPage.tsx
@@ -8,49 +8,8 @@ import HitPoints from "../components/characterSheet/HitPoints";
 import SecondarySkills from "../components/characterSheet/SecondarySkills";
 import Proficiencies from "../components/characterSheet/Proficiencies";
 import CharacterTitle from "../components/characterSheet/CharacterTitle";
-import backgroundImage from "../assets/papyrus.jpg";
 import Feats from "../components/characterSheet/Feats";
-
-const styles: { [key: string]: React.CSSProperties } = {
-	container: {
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		padding: "20px",
-		backgroundColor: "#f9f9f9",
-		minHeight: "100vh",
-		boxSizing: "border-box",
-		backgroundImage: `url(${backgroundImage})`,
-	},
-	ribbonContainer: {
-		display: "flex",
-		justifyContent: "center",
-		width: "100%",
-		marginBottom: "20px",
-	},
-	grid: {
-		display: "grid",
-		gridTemplateColumns: "1fr 1fr",
-		gap: "20px",
-		width: "100%",
-		maxWidth: "1200px",
-	},
-	leftColumn: {
-		gridColumn: "1 / 2",
-		display: "flex",
-		flexDirection: "column",
-		gap: "20px",
-	},
-	rightColumn: {
-		gridColumn: "2 / 3",
-		display: "flex",
-		flexDirection: "column",
-		gap: "20px",
-	},
-	proficiencies: {
-		gridColumn: "1 / -1",
-	},
-};
+import "../styles/characterPage.css";
 
 const CharacterPage: React.FC = () => {
 	const {
@@ -65,26 +24,26 @@ const CharacterPage: React.FC = () => {
 	} = CharacterSheet;
 
 	return (
-		<div style={styles.container}>
-			<div style={styles.ribbonContainer}>
+		<div className="container">
+			<div className="ribbon-container">
 				<RibbonTitle title={title.name} />
 			</div>
-			<div style={styles.fullWidth}>
+			<div className="full-width">
 				<CharacterTitle title={title} />
 			</div>
 			<MainSkills mainSkills={mainSkills} />
 			<CharacterAttributes attributes={characterAtributes} />
 
-			<div style={styles.grid}>
-				<div style={styles.leftColumn}>
+			<div className="grid">
+				<div className="left-column">
 					<SecondarySkills skills={secondarySkills} />
 				</div>
-				<div style={styles.rightColumn}>
+				<div className="right-column">
 					<HitPoints hitPoints={hitPoints} />
 					<SavingThrows throws={savingThrows} />
 					<Feats feats={feats} />
 				</div>
-				<div style={styles.proficiencies}>
+				<div className="proficiencies">
 					<Proficiencies proficiencies={proficiencies} />
 				</div>
 			</div>
