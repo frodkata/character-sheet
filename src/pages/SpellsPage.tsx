a78856d42fe2a9a004157403bcadd851a50b5741
frodkata <frodkata@gmail.com>
2025-04-24 15:48:29 +0300

    Add spell slots display

--- src/pages/SpellsPage.tsx
+++ src/pages/SpellsPage.tsx
@@ -3,6 +3,7 @@ import { CharacterSheet } from "../data/CharacterSheet";
 import Spell from "../components/Spell";
 import backgroundImage from "../assets/papyrus.jpg";
 import { Link } from "react-router-dom";
+import D20 from "../components/svg components/D20";
 
 const styles: { [key: string]: React.CSSProperties } = {
 	page: {
@@ -18,6 +19,38 @@ const styles: { [key: string]: React.CSSProperties } = {
 		fontSize: "2.5rem",
 		textShadow: "1px 1px 2px #000",
 		marginBottom: "20px",
+	},
+	spellSlotsContainer: {
+		display: "flex",
+		flexWrap: "wrap",
+		gap: "10px",
+		justifyContent: "center",
+		marginBottom: "20px",
+	},
+	spellSlotItem: {
+		padding: "10px",
+		borderRadius: "6px",
+		backgroundColor: "#222222",
+		boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+		fontSize: "1rem",
+		color: "#fff",
+		fontWeight: "bold",
+		minWidth: "120px",
+		fontFamily: "'Cinzel', serif",
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	spellSlotLevel: {
+		color: "#f4e4c1", // Parchment-like text color
+		fontWeight: "bold",
+		marginRight: "10px",
+		marginLeft: "10px",
+	},
+	spellSlotValue: {
+		color: "#FFA500", // Orange
+		fontWeight: "bold",
 	},
 	spellList: {
 		listStyleType: "none",
@@ -36,16 +69,44 @@ const styles: { [key: string]: React.CSSProperties } = {
 		boxShadow: "0 2px 4px rgba(0, 0, 0, 0.3)",
 		marginRight: 10,
 	},
+	tip: {
+		textAlign: "center",
+		color: "#8b0000",
+		fontSize: "2.5rem",
+		textShadow: "1px 1px 2px #000",
+		marginBottom: "20px",
+	},
 };
 
 const SpellsPage: React.FC = () => {
+	const spellSlots = {
+		"Level 1 slots": 4,
+		"Level 2 slots": 3,
+		"Level 3 slots": 3,
+		"Level 4 slots": 2,
+	};
+
 	const spells = CharacterSheet.spells;
+
 	return (
 		<div style={styles.page}>
 			<Link to="/spell-search" style={styles.link}>
 				Search for a Spell 🔍
 			</Link>
 			<h1 style={styles.header}>Spellbook</h1>
+			{/* Spell Slots Section */}
+			<div style={styles.spellSlotsContainer}>
+				{Object.entries(spellSlots).map(([level, slots]) => (
+					<div key={level} style={styles.spellSlotItem}>
+						<D20 fill="#FFA500" />
+						<span style={styles.spellSlotLevel}>{level}:</span>
+						<span style={styles.spellSlotValue}>{slots}</span>
+					</div>
+				))}
+			</div>
+
+			<span style={styles.tip}>+2</span>
+			{/* Spell List */}
 			<ul style={styles.spellList}>
 				{spells.map((spell, index) => (
 					<Spell
